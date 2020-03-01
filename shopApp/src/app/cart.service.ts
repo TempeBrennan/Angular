85fbcc90cf7c61f11815f90aac2772149b5e290f
AronChen <AronChen@XA-DD3-ARON>
2020-03-01 11:21:41 +0800

    add cart

---
+++ shopApp/src/app/cart.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { products } from './products';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CartService {
+    items = [];
+
+    constructor() { }
+
+    addToCart(product) {
+        this.items.push(product);
+    }
+
+    getItems() {
+        return this.items;
+    }
+
+    clearCart() {
+        this.items = [];
+        return this.items;
+    }
+}
