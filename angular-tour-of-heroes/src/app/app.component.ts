5344f27e31c5828d5a1b1774b33e682422c7e321
TempeBrennan <425751342@qq.com>
2020-03-16 21:38:04 +0800

    add custom element

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Injector } from '@angular/core';
+import { createCustomElement, NgElement, WithProperties } from '@angular/elements';
+import { PopupService } from './custom-element/popup.service';
+import { PopupComponent } from './custom-element/popup.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-tour-of-heroes';
+
+  message = 'aaa';
+
+  constructor(injector: Injector, public popup: PopupService) {
+
+    // 记得用 ng add @angular/elements，否则Edge不支持
+
+    // Convert `PopupComponent` to a custom element.
+    const PopupElement = createCustomElement(PopupComponent, { injector });
+    // Register the custom element with the browser.
+    customElements.define('popup-element', PopupElement);
+  }
 }
--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -14,6 +14,10 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
 import { MessagesComponent } from './messages/messages.component';
+import { PopupComponent } from './custom-element/popup.component';
+import { PopupService } from './custom-element/popup.service';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
@@ -27,7 +31,9 @@ import { MessagesComponent } from './messages/messages.component';
     // Remove it when a real server is ready to receive requests.
     HttpClientInMemoryWebApiModule.forRoot(
       InMemoryDataService, { dataEncapsulation: false }
-    )
+    ),
+
+    BrowserModule, BrowserAnimationsModule
   ],
   declarations: [
     AppComponent,
@@ -35,9 +41,11 @@ import { MessagesComponent } from './messages/messages.component';
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    HeroSearchComponent
+    HeroSearchComponent,
+    PopupComponent
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers:[PopupService]
 })
 
 export class AppModule { }
