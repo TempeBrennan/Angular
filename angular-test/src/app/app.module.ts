103e4c35422fa4aa6f1d2f61b008764a22d87249
TempeBrennan <425751342@qq.com>
2020-03-22 14:35:44 +0800

    add

--- angular-test/src/app/app.module.ts
+++ angular-test/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { SimpleTestComponent } from './simple-test/simple-test.component';
+import { MyTestComponent } from './my-test/my-test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SimpleTestComponent
+    SimpleTestComponent,
+    MyTestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
